8ecefe227f98e8632dd86e0e3203d52da2e01644
SeviMatePro <sevinci.chiamil@gmail.com>
2026-05-09 21:15:08 +0300

    Update get-tests.js

--- netlify/functions/get-tests.js
+++ netlify/functions/get-tests.js
@@ -1,6 +1,19 @@
 const { Client } = require('pg');
 
 exports.handler = async (event, context) => {
+  // Configurare permisiuni (CORS)
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Content-Type": "application/json"
+  };
+
+  // Răspuns rapid pentru verificările browserului
+  if (event.httpMethod === "OPTIONS") {
+    return { statusCode: 200, headers, body: "" };
+  }
+
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
@@ -12,17 +25,14 @@ exports.handler = async (event, context) => {
     
     return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Content-Type": "application/json"
-      },
+      headers,
       body: JSON.stringify(result.rows)
     };
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: err.message })
+      headers,
+      body: JSON.stringify({ error: "Eroare DB", details: err.message })
     };
   } finally {
     await client.end();
